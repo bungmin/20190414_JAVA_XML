8b0311e696e3f48798ed58e245d1b7da42911e69
501 <ha0801@nate.com>
2019-04-14 10:25:31 +0900

    bbbbb

--- app/src/main/java/com/hh/a20190414_java_xml/MainActivity.java
+++ app/src/main/java/com/hh/a20190414_java_xml/MainActivity.java
@@ -2,6 +2,7 @@ package com.hh.a20190414_java_xml;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("메인-onCreate테스트", "정말실행되나?");
+        Log.e("메인-ee  onCreate테스트", "정말실행되나?");
     }
 }
